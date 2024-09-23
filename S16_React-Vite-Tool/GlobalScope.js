36d4f96e2d31e70c5adfb7e014a96dde1d236ab4
Kabir <sagithedeveloper@gmail.com>
2024-09-23 21:02:34 +0530

    Added the code

---
+++ S16_React-Vite-Tool/GlobalScope.js
@@ -0,0 +1,14 @@
+var x = 10;
+let y = 100;
+const pin = 123;
+
+if (true) {
+  var city = "hyd";
+  let name = "rohan";
+  const password = 12345;
+}
+function f1() {
+  var price = 1000;
+}
+
+console.log(price);
